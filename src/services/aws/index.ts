065d6d83e3fefb34f2f92348e1fdb6c1369536bd
Mitch Downey <mitchldowney@gmail.com>
2023-12-10 23:44:37 -0600

    Add credentials params to AWSService

--- src/services/aws/index.ts
+++ src/services/aws/index.ts
@@ -1,22 +1,30 @@
 import aws from 'aws-sdk'
 
 type Constructor = {
-  key: string
+  accessKeyId: string
   region: string
+  secretAccessKey: string
 }
 
 export class AWSService  {
-  declare key: string
+  declare accessKeyId: string
   declare region: string
+  declare secretAccessKey: string
 
-  constructor ({ region }: Constructor) {
+  constructor ({ accessKeyId, region, secretAccessKey }: Constructor) {
+    this.accessKeyId = accessKeyId
     this.region = region
+    this.secretAccessKey = secretAccessKey
     
     aws.config.update({
       region: this.region,
       httpOptions: {
         connectTimeout: 5000,
         timeout: 5000
+      },
+      credentials: {
+        accessKeyId: this.accessKeyId,
+        secretAccessKey: this.secretAccessKey
       }
     })
   }
