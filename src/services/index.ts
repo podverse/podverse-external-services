149c0a00e12db3d77feeaeb992ccb9ce300eefd7
Mitch Downey <mitchldowney@gmail.com>
2025-11-20 02:38:11 -0600

    Update NotificationsService export to include SendNotificationOptions

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 // export { AWSS3Service, AWSSQSService } from './aws'
+export { GoogleFCMService } from './google/fcm';
 export { FirebaseAccessTokenService } from './google/firebaseAccessTokenService';
-export { NotificationsService } from './notifications';
+export { NotificationsService, SendNotificationOptions } from './notifications';
 export { PayPalService } from './paypal';
 export { PodcastIndexService } from './podcast-index';
 // export { UnifiedPushService } from './unifiedpush'
